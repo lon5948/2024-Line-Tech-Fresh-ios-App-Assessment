57e20945d30702b9c161be61d85733b9978ff94d
lon5948 <a0988282303@gmail.com>
2024-04-16 15:37:39 +0800

    Initial Commit

---
+++ 2024-Line-Tech-Fresh-ios-App-Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2024-Line-Tech-Fresh-ios-App-Assessment
+//
+//  Created by LON Li on 2024/4/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
